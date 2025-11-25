9fc0981fedcdbe992f36d63f0c8ca1ef759db457
Pantanosso <queralt.pau@gmail.com>
2025-11-25 16:03:40 +0100

    fix test integracio

--- test_integracion/test_busqueda_toni.py
+++ test_integracion/test_busqueda_toni.py
@@ -1,10 +1,7 @@
 from pages.login_page import LoginPage
 from pages.home_page import HomePage
 from pages.resultados_page import ResultadosPage
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+import time
 
 def test_busqueda_usuario(driver):
 
@@ -13,21 +10,14 @@ def test_busqueda_usuario(driver):
     resultados = ResultadosPage(driver)
 
     login.abrir()
+    time.sleep(1)
 
     login.login("toni@example.com", "app-fitness1")
+    time.sleep(2)
 
-    # wait until search input is visible (home ready)
-    WebDriverWait(driver, 15).until(
-        EC.visibility_of_element_located((By.XPATH, "//input[@placeholder='Buscar' or @aria-label='Buscar']"))
-    )
 
     home.buscar("Toni")
 
-    # Wait and open the result robustly
     resultados.abrir_resultado("Toni")
 
-    # Assert we landed on a user page and the user's name is visible
-    assert "/usuario/" in driver.current_url.lower()
-    WebDriverWait(driver, 10).until(
-        EC.text_to_be_present_in_element((By.TAG_NAME, "body"), "Toni")
-    )
+    assert "usuario/1" in driver.current_url.lower()
